a3cfd63fb4e04b1c6d2a10ffde8d52ab5d0dedcb
roman <sheva2012h@gmail.com>
2016-04-09 20:36:55 +0300

    Fix slides switching when number of last slides less then number of slides for display

--- js/syncSlider.js
+++ js/syncSlider.js
@@ -1,5 +1,5 @@
 // Will do in the future:
-// 1. fix nav functions when reverse is true
+// 1. add disable to nav buttons
 // 2. fix prev / next button when fix var is true
 // 3. add infinite support
 // 4. add fade animation support
@@ -7,6 +7,7 @@
 // 6. add css animation support
 // 7. add touch support
 // 8. add callbacks
+// * change o.play
 
 ;+function($) {
 
@@ -28,7 +29,7 @@
                  style: _('slider'),
               vertical: false,
               infinite: false,
-               reverse: false,//true,
+               reverse: false,
                   sync: false,
             switchTime: 1000,
               showTime: 1000,
@@ -45,7 +46,7 @@
         nav: {
                    use: true,                add: true,
                    tag: 'nav',           itemTag: 'a',
-                 style: _('nav'),
+                 style: _('nav'),   disableStyle: '-disable',
              prevStyle: _('prev'),     nextStyle: _('next'),
               prevText: 'Previous',     nextText: 'Next'
         },
@@ -54,8 +55,7 @@
                    use: true,                add: true,
                    tag: 'nav',           itemTag:  'a',
                  style: _('pager'),  activeStyle: '-active',
-              vertical: false,
-               stretch: true,
+               stretch: true,           vertical: false,
                numbers: true,
             switchMode: 2
         },
@@ -127,8 +127,11 @@
             n = N = ti.length;                  // set slides count
             l = N - o.display;                  // last possible slide index
             x = o.step;                         // set movement step
+            d = x > 0 ? 1 : -1;
+            x = d * x > l ? d * l : x;          // fix step if it more then last slide
             ax = x > 0 ? x : -x;                // absolutely step
-            fix = l < ax ? 0 : x % l;           // fix step is necessary
+            fix = l % ax || l < ax;             // fix step is necessary
+            i = I = o.startSlide - 1;           // set current slide
 
             // Add left and right navigation buttons
             if (o.nav.add) {
@@ -159,15 +162,15 @@
                     function(e) { $(e).css(_h, j); } :
                     function(e) { $(e).css(_w, j); };
                 var addClick = //fix ?
-                    // function(i, e) { $(e).on('click', function() { r.switchTo((i > l ? l : i) + 1) }); } :
-                    function(i, e) { $(e).on('click', function() { r.switchTo(             i  + 1) }); };
+                    // function(v, e) { $(e).on('click', function() { r.switchTo((v > l ? l : v) + 1) }); } :
+                    function(v, e) { $(e).on('click', function() { r.switchTo(             v  + 1) }); };
                 pi = p.children(o.pager.itemTag).each(o.pager.stretch ?
                     function(i, e) { addClick(i * ax, e); setSize(e); } :
                     function(i, e) { addClick(i * ax, e); });
                 // Set active start pager item
                 pi.eq(o.startSlide - 1).addClass(o.pager.activeStyle);
                 // Return current pager index
-                r.getCurrentPager = ax < 2 ?
+                r.getCurrentPage = ax < 2 ?
                     function() { return      i       + 1 } :
                     function() { return ceil(i / ax) + 1 };
                 // Convert current slide index to pager item index if display > 1
@@ -178,14 +181,14 @@
                 r.switchPagerTo = function(v) {
                     tgPager(toPager());
                     tgPager(v - 1);
-                    I = i;
+                    // I = i;
                 };
                 //
                 var setPager = o.pager.switchMode > 0 ?
                     function(v) { clearTimeout(j); j = setTimeout(function() {
                                   r.switchPagerTo(v); }, o.switchTime / o.pager.switchMode) } :
                     function(v) { r.switchPagerTo(v); };
-                j = function(v) { setSlide(v); setPager(r.getCurrentPager()); };
+                j = function(v) { setSlide(v); setPager(r.getCurrentPage()); };
             } else j = setSlide;
             r.switchTo = r.setSlide = j;
 
@@ -193,22 +196,15 @@
                 // Will do this in the future
                 l = N - o.display;
             } else {
-                d = x > 0 ? 1 : -1;
-                x = d * x > l ? d * l : x;
-
-                if (o.reverse) {
-                    calc = fix ? function() {
-                        i = d > 0 ? l - x * (i == l + x) : -x * (i == x); x *= -1;
-                    } : function() { i -= 2 * x; x *= -1; };
-                }
-                else {
-                    var lim = x > 0 ? l : 1,
-                        end = x < 0 ? l : 0;
-                    calc = fix ? function() { i = x > i - lim ? l : 0; } : function() { i = end; };
-                }
+                o.reverse ?
+                    calc = fix ?
+                        function() { i = d > 0 ? l - x * (i == l + x) : -x * (i == x); x *= -1; } :
+                        function() { i -= 2 * x; x *= -1; } :
+                    calc = fix ?
+                        function() { i = i - d * x < (!~d ? 1 : l) ? l : 0; } :
+                        function() { i = !~d ? l : 0; };
             }
 
-            i = I = o.startSlide - 1;      // set current slide
             t .css(si.de, -tis * i + '%'); // set tape start position
             t .css(si.ze,  tis * N + '%'); // set tape size
             ti.css(si.ze,  100 / N + '%'); // set tape items sizes
@@ -221,6 +217,7 @@
         }
 
         function setSlide(v) {
+            I = i;
             i = v - 1;
             (d = (i > l) - (i < 0)) ? calc() : 0; // start / end correction
             m[si.de] = -tis * i + '%'; // movement in a certain direction
